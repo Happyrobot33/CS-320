7f3078dc0dd782325934416928c69b9853b7d344
Matthew Herber <matthewherber@sbcglobal.net>
2025-02-07 23:05:47 -0500

    Functional assignment 1

--- Assignment 1/Triangle.cpp
+++ Assignment 1/Triangle.cpp
@@ -1,86 +1,71 @@
 #include <GL/glut.h>
 
-// Rotation angles for the cube man
-float angleX = 0.0f;
-float angleY = 0.0f;
-
-// Function to draw a cube
-void drawCube(float x, float y, float z, float width, float height, float depth) {
-    glPushMatrix();
-    glTranslatef(x, y, z);
-    glScalef(width, height, depth);
-    glutSolidCube(1.0);
-    glPopMatrix();
-}
-
-// Function to draw the cube man
-void drawCubeMan() {
-    // Body
-    drawCube(0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.5f);
-
-    // Head
-    drawCube(0.0f, 1.2f, 0.0f, 0.6f, 0.6f, 0.5f);
-
-    // Arms
-    drawCube(-0.8f, 0.3f, 0.0f, 1.2f, 0.3f, 0.3f);
-    drawCube(0.8f, 0.3f, 0.0f, 1.2f, 0.3f, 0.3f);
-
-    // Legs
-    drawCube(-0.3f, -1.2f, 0.0f, 0.3f, 1.2f, 0.3f);
-    drawCube(0.3f, -1.2f, 0.0f, 0.3f, 1.2f, 0.3f);
-}
+float x = 0;
+float y = 0;
+const float stepSize = 0.01f;
 
 // Display callback function
-void display() {
+void display()
+{
+    // set the clear color to blue
+    glClearColor(0, 0, 1, 0);
+    // clear the screen itself
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
 
-    // Move back to view the object
-    glTranslatef(0.0f, 0.0f, -6.0f);
+    // Translate based on the position
+    glTranslatef(x, y, 0.0f);
 
-    // Apply rotation
-    glRotatef(angleX, 1.0f, 0.0f, 0.0f);
-    glRotatef(angleY, 0.0f, 1.0f, 0.0f);
-
-    // Draw the cube man
-    drawCubeMan();
+    glBegin(GL_TRIANGLES);
+    glColor3f(1, 0, 0); // red
+    glVertex3f(0, 0.5, 0);
+    glVertex3f(-0.5, -0.5, 0);
+    glVertex3f(0.5, -0.5, 0);
+    glEnd();
 
     glutSwapBuffers();
 }
 
-// Timer callback for animation
-void timer(int value) {
-    angleX += 1.0f;
-    angleY += 1.0f;
+// keyboard callback function
+void handleInput(int key, int xin, int yin)
+{
+    //switch on the key
+    switch (key)
+    {
+        case GLUT_KEY_UP:
+            if (y < 0.5f)
+                y = y + stepSize;
+            break;
+        case GLUT_KEY_DOWN:
+            if (y > -0.5f)
+                y = y - stepSize;
+            break;
+        case GLUT_KEY_RIGHT:
+            if (x < 0.5f)
+                x = x + stepSize;
+            break;
+        case GLUT_KEY_LEFT:
+            if (x > -0.5f)
+                x = x - stepSize;
+            break;
+        default:
+            break;
+    }
 
-    if (angleX > 360.0f) angleX -= 360.0f;
-    if (angleY > 360.0f) angleY -= 360.0f;
-
+    //redisplay
     glutPostRedisplay();
-    glutTimerFunc(16, timer, 0);
-}
-
-// Reshape callback
-void reshape(int w, int h) {
-    glViewport(0, 0, w, h);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(45.0, (double)w / (double)h, 1.0, 100.0);
-    glMatrixMode(GL_MODELVIEW);
 }
 
 // Main function
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
     glutInitWindowSize(800, 600);
-    glutCreateWindow("Rotating Cube Man");
+    glutCreateWindow("Assignment 1");
 
-    glEnable(GL_DEPTH_TEST);
-
+    glutSpecialFunc(handleInput);
     glutDisplayFunc(display);
-    glutReshapeFunc(reshape);
-    glutTimerFunc(16, timer, 0);
 
     glutMainLoop();
     return 0;
